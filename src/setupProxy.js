a135fd7473f64a03b572e0eaa9d2cc9b892046e6
Agnola Tommaso <tommaso.agnola@supsi.ch>
2024-05-14 08:54:52 +0200

    feat 🦄: Update dfx config

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api/v2/',
         createProxyMiddleware({
-            target: 'http://localhost:4943/api/v2/',
+            target: 'http://127.0.0.1:4943/api/v2/',
             changeOrigin: true,
         })
     )
