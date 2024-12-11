5253ef25ec29cff215e8da2a0b32c6aed87b903c
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-12-11 16:52:27 +0100

    wip 🚧:  Trying

--- ci/scripts/merge-env-files.sh
+++ ci/scripts/merge-env-files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-awk -F = '!/^#/ {a[$1]=$2}END{for(i in a) print i "=" a[i]}' .env.ci $ENV_FILE > .env
+awk -F= '/^[[:alpha:]]/ {a[$1]=$2}END{for(i in a) print i "=" a[i]}' .env.ci $ENV_FILE > .env
 
 cat .env
