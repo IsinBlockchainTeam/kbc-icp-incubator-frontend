8bdfb58495055a6ceca386ef4ffed29e55a86bd1
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-12-11 16:19:55 +0100

    wip 🚧:  Trying

---
+++ ci/scripts/deploy-ic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+echo "BASE_DIR: $BASE_DIR"
+
+cd ..
+
+# Ping dfx network
+dfx ping ic
+
+# Source .env
+set -o allexport
+source .env
+set +o allexport
+
+dfx deploy kbc-platform --network ic --yes
+
+# Canister status
+dfx canister status kbc-platform --network ic
