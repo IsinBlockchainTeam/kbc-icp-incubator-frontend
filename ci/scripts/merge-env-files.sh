c66404b4e4c68b720ef372826787682e37b9165a
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-12-11 16:23:26 +0100

    wip 🚧:  Trying

--- ci/scripts/merge-env-files.sh
+++ ci/scripts/merge-env-files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cat .env.ci
+cat $ENV_FILE
+
 awk 'NF {if ($0 ~ /^[^#]/) {key = substr($0, 1, index($0, "=") - 1); if (!seen[key]++) {print $0}} else {print $0}}' .env.ci $ENV_FILE > .env
 
 cat .env
